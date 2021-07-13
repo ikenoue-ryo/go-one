0f8eb1b0f7f1bd8284972d3718d3940086652f58
ikenoue-ryo <ikenoue.ryo@gmail.com>
2021-07-13 22:46:01 +0900

    htmlレンダリング

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import (
-	"go-one/app/controllers"
-)
+import "go-one/app/controllers"
 
 func main() {
 	controllers.StartMainServer()
+	// fmt.Println(models.Db)
+
+	// t := &models.Todo{}
+	// t.Content = "test content"
+	// t.UserID = 1
+	// fmt.Println(t)
+
+	// u := &models.User{}
+	// u.Name = "ryo"
+	// u.Email = "ryo@gmail.com"
+	// u.CreateTodo("First todo")
 }
