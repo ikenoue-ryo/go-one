4295563edd9a51c91829a7097eb988bf13bd7d6f
ikenoue-ryo <ikenoue.ryo@gmail.com>
2021-08-29 19:07:39 +0900

    echoでrestAPI開発

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
-	"go-one/go-backend/controllers"
+	"go-one/go-backend/database"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
+// import (
+// 	"go-one/go-backend/controllers"
+// )
+
+// func main() {
+// 	controllers.StartMainApiServer()
+// }
+
+type Category struct {
+	Id   int    `json:"id" param:"id"`
+	Name string `json:"name"`
+	Kana string `json:"kana"`
+}
+
+var categories = []Category{
+	{
+		Id:   1,
+		Name: "椅子",
+		Kana: "イス",
+	},
+	{
+		Id:   2,
+		Name: "机",
+		Kana: "ツクエ",
+	},
+	{
+		Id:   3,
+		Name: "ベッド",
+		Kana: "ベッド",
+	},
+}
+
+func getCategory(c echo.Context) error {
+	return c.JSON(http.StatusOK, categories)
+}
+
 func main() {
-	controllers.StartMainApiServer()
+	e := echo.New()
+	database.Connect()
+	sqlDB, _ := database.DB.DB()
+	defer sqlDB.Close()
+
+	e.GET("/category", getCategory)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
