7892cef3260a67c64e2757e4a925efa9d481e284
ikenoue-ryo <ikenoue.ryo@gmail.com>
2021-09-05 07:30:02 +0900

    frontをvueで開発

---
+++ frontend/src/components/categories.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    {{categories}}
+
+    <div v-for="category in categories" :key="category.id">
+      {{category.id}} {{ category.name }} {{ category.kana }}
+    </div>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  data(){
+    return {
+      msg:'Hello TypeScript',
+      message: '',
+      categories: ''
+  }},
+  mounted() {
+      axios
+        .get('/category')
+        .then(response => {
+          this.categories = response.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  }
+})
+</script>
